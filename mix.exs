ca3b24cef3335570487aeba54b605161bfd6270f
Tom Haines <tom@tomhaines.xyz>
2020-07-07 11:30:47 +0100

    Attempt CI

--- mix.exs
+++ mix.exs
@@ -10,7 +10,9 @@ defmodule SmartHomeAuth.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -43,7 +45,9 @@ defmodule SmartHomeAuth.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:auth_plug, git: "https://github.com/dwyl/auth_plug", branch: "master"}
+      {:auth_plug, git: "https://github.com/dwyl/auth_plug", branch: "master"},
+
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
