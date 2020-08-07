e09f5b3ea40b6ef89f7996a0e61217f34b0bb141
nelsonic <nelson+github@dwyl.io>
2020-08-07 22:16:29 +0100

    set host: "dwylauth.herokuapp.com" in config/prod.exs for #14

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :smart_home_auth, SmartHomeAuthWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "dwylauth.herokuapp.com/", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
