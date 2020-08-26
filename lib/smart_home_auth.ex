9ee32aa47ec77f6e3069543834cbc65803d8d059
Tom Haines <tom@tomhaines.xyz>
2020-08-26 14:25:20 +0100

    #21: Generate JWT locally with Mix task

--- lib/smart_home_auth.ex
+++ lib/smart_home_auth.ex
@@ -7,4 +7,18 @@ defmodule SmartHomeAuth do
   if it comes from the database, an external API or others.
 
   """
+
+  @doc """
+  Generate a local-use authentication token for API development.
+  """
+  @spec gen_auth_token :: binary
+  def gen_auth_token do
+    AuthPlug.Token.generate_jwt!(
+      %{
+        "id" => 1,
+        "email" => "test@example.com",
+        "auth_provider" => "local-dev",
+        "given_name" => "Dev Superuser"
+      })
+  end
 end
